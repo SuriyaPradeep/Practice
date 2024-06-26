563ae36021a78ff1218aeb0c8f43d1c0a896023b
Suriya <122858374+SuriyaPradeep@users.noreply.github.com>
2024-06-26 11:01:00 +0530

    Create MaxConsecutiveOnes.java

---
+++ src/SUMS/MaxConsecutiveOnes.java
@@ -0,0 +1,24 @@
+package SUMS;
+
+public class MaxConsecutiveOnes {
+    /*
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+     */
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCons=0,currCons=0;
+        for(int num:nums){
+            if(num!=1){
+                currCons=0;
+            }else{
+                currCons++;
+            }
+            maxCons=Math.max(maxCons,currCons);
+        }
+        return maxCons;
+    }
+}
