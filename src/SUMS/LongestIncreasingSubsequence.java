2d9787c8b014480ff075f25ab9ef557d0d7dc3ef
Suriya <122858374+SuriyaPradeep@users.noreply.github.com>
2024-06-27 19:09:22 +0530

    Create LongestIncreasingSubsequence.java

---
+++ src/SUMS/LongestIncreasingSubsequence.java
@@ -0,0 +1,29 @@
+package SUMS;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+    /*
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+Example 1:
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+Example 2:
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+     */
+    public int lengthOfLIS(int[] nums) {
+        int n= nums.length;
+        int[] dp=new int[n];
+        Arrays.fill(dp,1);
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+}
